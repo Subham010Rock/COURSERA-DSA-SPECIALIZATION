b19baba1ec50984a2fecfdd9db3586cc19407e3c
Subham Kumar <ssshubhamshrivastav010@gmail.com>
2022-04-15 22:52:26 +0530

    Week 4 added

--- Algorithmic Toolbox/Week 4/binary_search_sol.cpp
+++ Algorithmic Toolbox/Week 4/binary_search_sol.cpp
@@ -5,9 +5,19 @@
 using std::vector;
 
 int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
+  int left = 0, right = (int)a.size()-1; 
   //write your code here
-  
+  int mid;
+  while(left<=right){
+    mid=(left+right)/2;
+    if(a[mid]==x)
+      return mid;
+    else if(a[mid]>x)
+      right=mid-1;
+    else
+      left=mid+1;
+  }
+  return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -32,6 +42,6 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search(a, b[i]) << ' ';
   }
 }
